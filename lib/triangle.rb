d9c2e33c1c8e968ad27ae24242fa48bc4705302a
Angela Lo <angelawenlo@gmail.com>
2019-07-01 13:28:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,8 +14,8 @@ class Triangle
     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
    raise TriangleError
    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+        binding.pry
    raise TriangleError
-   binding.pry
    else
    if (@side_1 == @side_2) && (@side_2 == @side_3)
      :equilateral
