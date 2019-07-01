5a1cbf47bef2e4a0131054182d831f390467ca42
Angela Lo <angelawenlo@gmail.com>
2019-07-01 13:05:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,13 @@
 class Triangle
   # write code here
+  #attr_accessor :equilateral, :isosceles, :scalene
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+  end
 end
